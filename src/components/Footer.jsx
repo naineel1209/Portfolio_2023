2d9f79fc20b0a217a95b6dc790c81e65daeaa512
Naineel <naineelsoyantar@gmail.com>
2023-05-29 10:33:01 +0530

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,22 +14,22 @@ const Footer = props => {
 
             </div> */}
 
-            <div className='mt-10 h-full'>
-                <p className='text-center text-3xl font-bold border-b-4 border-gray-500'>Socials</p>
-                <div className='mt-5 grid grid-cols-4 gap-10 text-center text-2xl text-gray-500 '>
+            <div className='h-full'>
+                <p className='text-center text-3xl font-bold border-b-4 border-gray-500'>Credits</p>
+                <div className='my-10 text-center text-xl text-gray-500 '>
+                    Image: <a className='flex flex-row gap-3 items-center justify-center hover:text-white duration-200' href="https://twitter.com/TransAlchemy">[P]articleion <FaTwitter />
+                    </a>
+                </div>
+            </div>
+            <div className='h-full'>
+                <p className='text-center text-3xl font-bold border-b-4 border-gray-500 inline-'>Socials</p>
+                <div className='my-10 grid grid-cols-4 gap-10 text-center text-xl text-gray-500 '>
                     <a className='flex flex-row gap-3 items-center justify-center hover:text-white duration-200' href="https://twitter.com/"> <FaTwitter /> </a>
                     <a className='flex flex-row gap-3 items-center justify-center hover:text-white duration-200' href="https://www.instagram.com/_naineel/"> <FaInstagramSquare />
                     </a>
                     <a className='flex flex-row gap-3 items-center justify-center hover:text-white duration-200' href="https://www.linkedin.com/in/naineel-soyantar-3b1b371b7/"> <FaLinkedin />
                     </a>
                     <a className='flex flex-row gap-3 items-center justify-center hover:text-white duration-200' href="https://github.com/naineel1209"> <FaGithub />
-                    </a>
-                </div>
-            </div>
-            <div>
-                <p className='mt-10 text-center text-3xl font-bold border-b-4 border-gray-500'>Credits</p>
-                <div className='mt-5 text-center text-xl text-gray-500 '>
-                    Image: <a className='flex flex-row gap-3 items-center justify-center hover:text-white duration-200' href="https://twitter.com/TransAlchemy">[P]articleion <FaTwitter />
                     </a>
                 </div>
             </div>
